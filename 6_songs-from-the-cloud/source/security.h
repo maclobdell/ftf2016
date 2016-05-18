2f75d40d21f8a6a8edbe154adbbc2bd57e478332
maclobdell <maclain.lobdell@arm.com>
2016-05-18 14:55:59 -0500

    remove mbed code and instructions.  this is now at https://developer.mbed.org/teams/MakingMusicWorkshop/

--- 6_songs-from-the-cloud/source/security.h
+++
@@ -1,1 +0,0 @@
-#error "No security.h file, go to https://connector.mbed.com/#credentials to get credentials. Then put it in sxsw/6_songs-from-the-cloud/source/security.h."
--- 8_sequence/source/security.h
+++
@@ -1,1 +0,0 @@
-#error "No security.h file, go to https://connector.mbed.com/#credentials to get credentials. Then put it in sxsw/8_sequence/source/security.h."
